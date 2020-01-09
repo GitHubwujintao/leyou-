0fb27c796f465539cc8b86e9f8dadc8eabc57a73
jiwu <2538735621@qq.com>
2020-01-09 16:06:06 +0800

    搜索搭建

---
+++ ly-search/src/main/java/com/leyou/search/client/GoodsClient.java
@@ -0,0 +1,8 @@
+package com.leyou.search.client;
+
+import com.leyou.api.GoodsApi;
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(value = "item-service")
+public interface GoodsClient extends GoodsApi {
+}
---
+++ ly-search/src/main/java/com/leyou/search/pojo/SearchResult.java
@@ -0,0 +1,36 @@
+package com.leyou.search.pojo;
+
+import com.leyou.common.vo.PageResult;
+import com.leyou.pojo.Brand;
+import com.leyou.pojo.Category;
+import lombok.Data;
+import java.util.List;
+import java.util.Map;
+
+
+@Data
+public class SearchResult extends PageResult<Goods> {
+
+    private List<Category> categories;
+
+    private List<Brand> brands;
+
+    private List<Map<String,Object>> specs; // 规格参数过滤条件
+
+    public SearchResult(){};
+
+    public SearchResult(Long total, Integer totalPage, List<Goods> items, List<Category> categories, List<Brand> brands) {
+        super(total, totalPage, items);
+        this.categories = categories;
+        this.brands = brands;
+    }
+
+
+
+    public SearchResult(Long total, Integer totalPage, List<Goods> items, List<Category> categories, List<Brand> brands, List<Map<String, Object>> specs) {
+        super(total, totalPage, items);
+        this.categories = categories;
+        this.brands = brands;
+        this.specs = specs;
+    }
+}
